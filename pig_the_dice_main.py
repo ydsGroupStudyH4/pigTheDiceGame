47d636c10d9f85846ca1017793e200319c2dac90
easydong02 <easydong02@gmail.com>
2022-05-09 21:02:16 +0900

    feat: change the name, code of func

--- pig_the_dice_main.py
+++ pig_the_dice_main.py
@@ -1,3 +1,14 @@
-def nPersons(n):
-    return n
+def make_n_persons():
+    while True:
+        n= int(input('Please set the number of people'))
+        if n>0:
+            if>4:
+                print("Please set the number under 4")
+            else:
+                print(f"The number of people is: {n}")
+                return n
+        else:
+            print("you typed wrong number, please do it again.")
+
+    
 
